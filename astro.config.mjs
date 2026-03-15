9794dbfcff4d4c929c44bc1195eaad546ff1e73b
AkasakaNarito <p605098@gmail.com>
2026-03-15 21:23:25 +0900

    finished: true

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://firstweba.netlify.app/",
-  integrations: [preact()]
 });
